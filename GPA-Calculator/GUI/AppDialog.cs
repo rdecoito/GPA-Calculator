7db3e73c6e9864f9061ac123b94e98aee99f6100
Robert Landon Decoito <rdecoito@GMU.EDU>
2018-08-22 01:14:34 -0400

    Added GetGPAFromString method to AppDialog and implemented it in a more streamlined btnCalculate_Click event handler

--- GPA-Calculator/GUI/AppDialog.cs
+++ GPA-Calculator/GUI/AppDialog.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GPA_Calculator.Util;
 
 namespace GPA_Calculator.GUI
 {
@@ -20,8 +21,9 @@ namespace GPA_Calculator.GUI
             m_tbCourseNameList[CourseCount - 1].Location.Y  + m_tbCourseNameList[CourseCount - 1].Size.Height : 0;
         public int CourseCount = 0;
         public int LastTabIndex = 0;
-
-
+        public GradeScale SelectedScale = new Util.FourPointGradeScale();
+        
+        
         public AppDialog()
         {
             InitializeComponent();
@@ -34,17 +36,41 @@ namespace GPA_Calculator.GUI
             return true;
         }
 
+        private double GetGPAFromString(string grade)
+        {
+            double numericGrade;
+            bool success = double.TryParse(grade, out numericGrade);
+            if (success)
+            {
+                IEnumerable<double> minGrades = SelectedScale.GradeLowestNumericMapping.Keys
+                    .OrderByDescending(d => d);
+                foreach (double minGrade in minGrades)
+                {
+                    if (numericGrade >= minGrade)
+                        return SelectedScale.GradeLowestNumericMapping[minGrade];
+                }
+            }
+
+            grade = grade.ToUpper();
+            IEnumerable<string> letterGrades = SelectedScale.GradeLetterMapping.Keys;
+            foreach (string letterGrade in letterGrades)
+            {
+                if (grade == letterGrade)
+                    return SelectedScale.GradeLetterMapping[letterGrade];
+            }
+
+            return -1;
+        }
+
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             if (!ValidateCourses())
                 return;
 
             int totalCredits = 0;
-            for (int i = 0; i < CourseCount; i++)
-            {
-                TextBox tb = m_tbCreditsList[i];
-                totalCredits += tb.Text != Program.NULL_STR ? Convert.ToInt32(tb.Text) : 0;
-            }
+            foreach (TextBox tb in m_tbCreditsList)
+                totalCredits += tb.Text != AppUtil.NULL_STR ? Convert.ToInt32(tb.Text) : 0;
+
             if (totalCredits <= 0)
             {
                 tbGPA.Text = "No Credits!";
@@ -54,13 +80,12 @@ namespace GPA_Calculator.GUI
             double totalWeightedGrade = 0;
             for (int i = 0; i < CourseCount; i++)
             {
-                double courseWeightedGrade = (m_tbGradeList[i].Text != Program.NULL_STR ?
-                    double.Parse(m_tbGradeList[i].Text) : 0)
-                    * (m_tbCreditsList[i].Text != Program.NULL_STR ? Convert.ToInt32(m_tbCreditsList[i].Text) : 0);
+                double courseWeightedGrade = GetGPAFromString(m_tbGradeList[i].Text)
+                    * (m_tbCreditsList[i].Text != AppUtil.NULL_STR ? Convert.ToInt32(m_tbCreditsList[i].Text) : 0);
                 totalWeightedGrade += courseWeightedGrade;
             }
 
-            tbGPA.Text = Math.Round((totalWeightedGrade / totalCredits), 3).ToString();
+            tbGPA.Text = Math.Round((totalWeightedGrade / totalCredits), 3, MidpointRounding.AwayFromZero).ToString();
         }
 
         private void btnAddCourse_Click(object sender, EventArgs e)
